5e01a4a99e8489367cf5b968f8d678b418f1ac8f
unknown <pruetikorn.junt@bumail.net>
2021-01-28 04:36:39 +0700

    Send Assignment Week 1

---
+++ 1610706614/Assets/YELLOW.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class YELLOW : MonoBehaviour
+{
+    public string n;
+    public GameObject ip;
+    public GameObject op;
+    public void ListName()
+    {
+
+        n = ip.GetComponent<Text>().text;
+        if (n == "Unity" || n == "Unreal" || n == "BOB" || n =="Google"||n=="MongoDB")
+        {
+            op.GetComponent<Text>().text = "<color=green>"+n+"</color>"+ " is found";
+        }
+        
+        else
+        {
+            op.GetComponent<Text>().text = "<color=red>" + n + "</color>" + " isnot found";
+        }
+
+    }
+ }
